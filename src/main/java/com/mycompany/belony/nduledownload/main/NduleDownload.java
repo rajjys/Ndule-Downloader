a98e8594bb511e5c084f040f1039b6aafdef57f2
Jonathan Idy Rachid <Jonathan Idy@DESKTOP-FEIKEJ4>
2022-08-02 19:48:26 +0200

    Main window added

--- src/main/java/com/mycompany/belony/nduledownload/main/NduleDownload.java
+++ src/main/java/com/mycompany/belony/nduledownload/main/NduleDownload.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.belony.nduledownload;
+package com.mycompany.belony.nduledownload.main;
 
 /**
  *
@@ -12,7 +12,7 @@ package com.mycompany.belony.nduledownload;
 public class NduleDownload {
 
     public static void main(String[] args) {
-        ///A comment to start the day
+        ///A comment to start the day 
         System.out.println("Hello World Johnny!");
     }
 }
