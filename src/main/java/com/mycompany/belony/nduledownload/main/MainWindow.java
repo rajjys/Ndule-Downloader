4d71835ea84323e46acb9189712d1391df09836a
Jonathan Idy Rachid <Jonathan Idy@DESKTOP-FEIKEJ4>
2022-08-02 19:47:50 +0200

    Main window

---
+++ src/main/java/com/mycompany/belony/nduledownload/main/MainWindow.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belony.nduledownload.main;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Jonathan Idy
+ */
+public class MainWindow extends JFrame {
+    ///Main Window components
+    private JTextField queryField;
+    private JButton queryActionBtn;
+    private JPanel contentPanel;
+    
+    
+    ///Constructor
+    public MainWindow(){
+        initComponents();
+    }
+
+    private void initComponents() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
