35730bf788ad64c288bd6a3f9ca51dc0e3f883c1
Jonathan Idy Rachid <Jonathan Idy@DESKTOP-FEIKEJ4>
2022-08-25 11:52:49 +0200

    Added serialization to the download manager. This way, it saves the download list from previous session to the actual Next up we're going to implement youtube search

--- src/main/java/com/mycompany/belony/nduledownload/main/APIKey.java
+++ src/main/java/com/mycompany/belony/nduledownload/main/APIKey.java
@@ -10,5 +10,5 @@ package com.mycompany.belony.nduledownload.main;
  */
 ///To hold the Youtube API Key
 public class APIKey {
-    public static String API_Key = "AIzaSyA2om6XXmMgFmteccIAgB6Sej_dE0O9r-U"; ////Find your own API key from developpers.google.com
+    public static String API_Key = ""; ////Find your own API key from developpers.google.com
 }
